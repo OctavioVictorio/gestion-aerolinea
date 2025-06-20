61730235dca4f79162839b90a97b3e501ab773f7
OctavioVictorio <octavionvictorio@gmail.com>
2025-06-20 16:44:09 -0300

    creacion de models y admin en vuelos

--- vuelos/admin.py
+++ vuelos/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from vuelos.models import (
+    Vuelo,
+)
+
+
+@admin.register(Vuelo)
+class VueloAdmin(admin.ModelAdmin):
+    list_display = ('origen', 'destino', 'fecha_salida', 'precio_base', 'estado')
+    list_filter = ('origen', 'destino', 'estado')
+    search_fields = ('origen', 'destino')
--- vuelos/models.py
+++ vuelos/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Vuelo(models.Model):
+    ESTADO_VUELO = [
+        ('programado', 'Programado'),
+        ('en_vuelo', 'En vuelo'),
+        ('finalizado', 'Finalizado'),
+        ('cancelado', 'Cancelado'),
+    ]
+
+    avion = models.ForeignKey('flota.Avion', on_delete=models.CASCADE)
+    origen = models.CharField(max_length=100)
+    destino = models.CharField(max_length=100)
+    fecha_salida = models.DateTimeField()
+    fecha_llegada = models.DateTimeField()
+    duracion = models.DurationField()
+    estado = models.CharField(max_length=20, choices=ESTADO_VUELO)
+    precio_base = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"Vuelo {self.origen} → {self.destino} - {self.fecha_salida.strftime('%Y-%m-%d %H:%M')}"
