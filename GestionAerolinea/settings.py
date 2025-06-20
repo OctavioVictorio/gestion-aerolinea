6770299e2c3985a218e720218a4248d2a931564c
OctavioVictorio <octavionvictorio@gmail.com>
2025-06-20 14:44:50 -0300

    Creacion de apps

--- GestionAerolinea/settings.py
+++ GestionAerolinea/settings.py
@@ -37,6 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #apps agregadas
+    'flota',
+    'vuelos',
+    'reservas',
+    'usuarios',
+    'reportes',
+    'pasajeros',
 ]
 
 MIDDLEWARE = [
