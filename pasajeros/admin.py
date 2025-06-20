92df41bebfc07e49e9c240de11c7131eb492ff5f
OctavioVictorio <octavionvictorio@gmail.com>
2025-06-20 17:14:24 -0300

    modificacion de models y admin en pasajeros

--- pasajeros/admin.py
+++ pasajeros/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from pasajeros.models import (
+    Pasajero,
+)
+
+@admin.register(Pasajero)
+class PasajeroAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'documento', 'tipo_documento', 'email', 'telefono', 'fecha_nacimiento')
+    list_filter = ('nombre','documento',)
+    search_fields = ('nombre', 'documento', 'email',)
--- pasajeros/models.py
+++ pasajeros/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Pasajero(models.Model):
+    Tipo_Documentos = [
+        ('dni', 'DNI'),
+        ('pasaporte', 'Pasaporte'),
+        ('cedula', 'Cédula'),
+    ]
+
+    nombre = models.CharField(max_length=100)
+    documento = models.CharField(max_length=20, unique=True)
+    tipo_documento = models.CharField(max_length=20, choices=Tipo_Documentos)
+    email = models.EmailField()
+    telefono = models.CharField(max_length=20)
+    fecha_nacimiento = models.DateField()
+
+    def __str__(self):
+        return self.nombre
