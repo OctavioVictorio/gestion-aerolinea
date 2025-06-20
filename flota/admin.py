46a44e28c960c59492dce3af8c6565ecfe464250
OctavioVictorio <octavionvictorio@gmail.com>
2025-06-20 15:59:02 -0300

    creacion de models y admin en Flota

--- flota/admin.py
+++ flota/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
 # Register your models here.
+from flota.models import (
+    Asiento,
+    Avion,
+)
+
+
+@admin.register(Avion)
+class AvionAdmin(admin.ModelAdmin):
+    list_display = ('modelo', 'capacidad', 'filas', 'columnas')
+    search_fields = ('modelo',)
+    list_filter = ('modelo',)
+
+
+@admin.register(Asiento)
+class AsientoAdmin(admin.ModelAdmin):
+    list_display = ('avion', 'numero', 'fila', 'columna', 'tipo', 'estado')
+    search_fields = ('numero', 'fila', 'columna', 'tipo')
+    list_filter = ('avion', 'tipo', 'estado')
--- flota/models.py
+++ flota/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+class Avion (models.Model):
+    modelo = models.CharField(max_length=100)
+    capacidad = models.PositiveIntegerField()
+    filas = models.PositiveIntegerField()
+    columnas = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f'{self.modelo} {self.capacidad} asientos'
+    
+
+class Asiento (models.Model):
+    TIPO_ASIENTO = [
+        ('economico', 'Económico'),
+        ('business', 'Business'),
+        ('primera', 'Primera clase'),
+    ]
+
+    ESTADO_ASIENTO = [
+        ('disponible', 'Disponible'),
+        ('reservado', 'Reservado'),
+        ('ocupado', 'Ocupado'),
+    ]
+
+
+    avion = models.ForeignKey(Avion, on_delete=models.CASCADE)
+    numero = models.PositiveIntegerField()
+    fila = models.PositiveIntegerField()
+    columna = models.PositiveIntegerField()
+    tipo = models.CharField(max_length=20, choices=TIPO_ASIENTO)
+    estado = models.CharField(max_length=20, choices=ESTADO_ASIENTO, default='disponible')
+
+
+    def __str__(self):
+        return f"Asiento {self.numero} - Fila {self.fila} Col {self.columna} ({self.tipo}) - {self.estado}"
